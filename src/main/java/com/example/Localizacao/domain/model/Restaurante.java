24a063500bab19b16cbefa0583ebed4d5d14dfca
cleysonbarros <cleysonbarros@yahoo.com.br>
2021-08-03 15:45:13 -0300

    Teste de Localizacao.

---
+++ src/main/java/com/example/Localizacao/domain/model/Restaurante.java
@@ -0,0 +1,41 @@
+package com.example.Localizacao.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import net.minidev.json.annotate.JsonIgnore;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Restaurante {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private BigDecimal taxaFrete;
+
+    @JsonIgnore
+    @CreationTimestamp
+    @Column(nullable = false, columnDefinition = "datetime")
+    private LocalDateTime dataCadastro;
+
+    @JsonIgnore
+    @UpdateTimestamp
+    @Column(nullable = false, columnDefinition = "datetime")
+    private  LocalDateTime dataAtualizacao;
+
+    @Embedded
+    private Endereco endereco;
+
+
+}

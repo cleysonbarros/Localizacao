a5605ad7d8d43f2d419ec00be85e2b399ffdcfe8
cleysonbarros <cleysonbarros@yahoo.com.br>
2021-08-03 15:52:25 -0300

    Teste de Localizacao.

--- src/main/java/com/example/Localizacao/domain/model/Restaurante.java
+++ src/main/java/com/example/Localizacao/domain/model/Restaurante.java
@@ -20,8 +20,10 @@ public class Restaurante {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String nome;
 
+    @Column(name = "taxa_frete", nullable = false)
     private BigDecimal taxaFrete;
 
     @JsonIgnore
